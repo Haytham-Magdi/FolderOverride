4186d22c4239e2652ad8b30d87e8ba24ab7a9590
haytham magdi <haytham.magdi@hotmail.com>
2016-06-04 15:32:36 +0300

    separating destination processing

---
+++ FolderOverride/ProcessElements/FileDestAction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderOverride.ProcessElements
+{
+    public class FileDestAction
+    {
+        public enum ActionType
+        {
+            Copy = 0,
+            Move,
+            Delete,
+        }
+
+        public FileInfo FileInfo { get; set; }
+        public ActionType Type { get; set; }
+        public string DestFullName { get; set; }
+
+        public void Execute()
+        {
+            switch (this.Type)
+            {
+                case ActionType.Copy:
+                    ExecuteCopy();
+                    break;
+                case ActionType.Move:
+                    ExecuteMove();
+                    break;
+                case ActionType.Delete:
+                    ExecuteDelet();
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        void ExecuteCopy()
+        {
+            this.FileInfo.CopyTo(this.DestFullName);
+        }
+
+        void ExecuteMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ExecuteDelet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FolderOverride/ProcessElements/FolderDestAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderOverride.ProcessElements
+{
+    public class FolderDestAction
+    {
+        public enum ActionType
+        {
+            Create = 0,
+            Delete,
+        }
+
+        //public DirectoryInfo DirectoryInfo { get; set; }
+        public ActionType Type { get; set; }
+        public string DestFullName { get; set; }
+    }
+}
